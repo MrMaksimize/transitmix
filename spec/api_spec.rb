deef2eb15cd5d1988a59a597d0995f838c625e52
Danny Whalen <daniel.r.whalen@gmail.com>
2014-05-03 11:34:16 -0700

    Test that the expected response body is returned

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -7,10 +7,6 @@ describe Transitmix::API do
     Transitmix::API
   end
 
-  def response_body
-    JSON.parse(last_response.body)
-  end
-
   describe 'GET /api/lines/:id' do
     let(:line) { create(:line) }
 
@@ -47,7 +43,7 @@ describe Transitmix::API do
     it 'returns the list of lines' do
       lines = create_list(:line, 5)
       get '/api/lines', per: 2
-      expect(response_body.first['id']).to eq lines.last.id
+      expect(last_response.body).to eq [lines[4], lines[3]].to_json
     end
   end
 end
